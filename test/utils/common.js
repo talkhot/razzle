542f23b8feeaa976bd3044f227c7c7cb018a8ca3
William Esz <flagaello@gmail.com>
2018-02-20 05:52:55 +0100

    Fix infinite promise in test utils

--- test/utils/common.js
+++ test/utils/common.js
@@ -1,9 +1,12 @@
 const shell = require('shelljs');
 const kill = require('../utils/psKill');
 
+shell.config.silent = true;
+
 exports.run = ({ main, print, matches }) =>
   new Promise((resolve, reject) => {
-    shell.exec(main).stdout.on('data', data => {
+    const child = shell.exec(main, { async: true });
+    child.stdout.on('data', data => {
       if (data.includes(matches[0])) {
         shell.exec('sleep 5');
         const output = shell.exec(print).stdout.includes(matches[1]);
