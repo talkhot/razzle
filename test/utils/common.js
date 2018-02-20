40e18b22a2653e731709f772af15ea21ec067414
William Esz <flagaello@gmail.com>
2018-02-20 07:20:17 +0100

    Fix test utils infinite promise and add rejection on stderr

--- test/utils/common.js
+++ test/utils/common.js
@@ -3,17 +3,21 @@ const kill = require('../utils/psKill');
 
 shell.config.silent = true;
 
-exports.run = ({ main, print, matches }) =>
-  new Promise((resolve, reject) => {
+exports.run = ({ main, print, matches }) => {
+  return new Promise((resolve, reject) => {
     const child = shell.exec(main, { async: true });
+    child.stderr.on('data', data => {
+      kill(child.pid);
+      reject(data);
+    });
     child.stdout.on('data', data => {
       if (data.includes(matches[0])) {
         shell.exec('sleep 5');
-        const output = shell.exec(print).stdout.includes(matches[1]);
+        const result = shell.exec(print).stdout.includes(matches[1]);
         kill(child.pid);
-        resolve(output);
-      } else {
-        reject();
+        resolve(result);
       }
+      // Do not reject on stdout as output may be continuous.
     });
   });
+};
